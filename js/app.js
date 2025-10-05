294dc628fa5606e487f4988012ae48224f7fff77
Kevin More Sandoval <kevinjesusmoresandoval@gmail.com>
2025-10-04 23:57:49 -0500

    Se agrego validaciones para los input de contraseña

--- js/app.js
+++ js/app.js
@@ -3,3 +3,13 @@ window.addEventListener("DOMContentLoaded", () => {
   const modal = new bootstrap.Modal(document.getElementById("promoModal"));
   modal.show();
 });
+
+const limitKey = 4;
+
+const concatenar = (id, number) => {
+  const input = document.getElementById(id);
+  let values = input.value.length;
+  if (values < limitKey) {
+    input.value += number;
+  }
+};
