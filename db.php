ca6ed10c5f7f0bb699b317408fab263625472efd
siguasjulcafernanda <fernandasiguas25@gmail.com>
2025-11-16 12:30:04 -0500

    PHP

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$passwordname = "";
+$password = "";
 $database = "contabilidad";
 
-$conn = new mysqli($servername, $username, $passwordname, $database);
+$conn = new mysqli($servername, $username, $passwor, $database);
 if($conn->connect_error){
     die("Error de Conexión");
 }
