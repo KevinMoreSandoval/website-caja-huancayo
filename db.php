8b7ccd1934f76f914e0b0494766f1eee09809e14
siguasjulcafernanda <fernandasiguas25@gmail.com>
2025-11-16 12:16:48 -0500

    php

--- db.php
+++ db.php
@@ -1,5 +1,12 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$passwordname = "";
+$database = "contabilidad";
 
-
+$conn = new mysqli($servername, $username, $passwordname, $database);
+if($conn->connect_error){
+    die("Error de Conexión");
+}
 
 ?>
--- guardar.php
+++ guardar.php
@@ -1,7 +1,18 @@
 <?php
+include('db.php');
+
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $fecha_nacimiento = $_POST['fecha_nacimiento'];
 $dni = $_POST['dni'];
 $contrasena = $_POST['contrasena'];
+ 
+//Insertar registros a tabla de la base de datos
+$sql="INSERT INTO usuarios(nombre,apellido,fecha_nacimiento,dni,contrasena) VALUES('$nombre', '$apellido', '$fecha_nacimiento', '$dni', '$contrasena')";
+ if($conn->query($sql) === TRUE) {
+    echo ('Usuario Registrado');
+ }else{
+    echo "Error";
+ }
+ $conn->close();
 ?>
